dbc7a946dc41be28dfe37185d4841c8be0d0b979
Uğur Arıcı <m.ugurarici@gmail.com>
2015-08-18 17:52:53 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# lyk2015_php-oop
+OOP eğitiminde gösterilenlerden bir demet.
+
+## Linux Yaz Kampı PHP Sınıfı OOP Dersleri
+
+- Sınıf oluşturma ve kullanmas
---
+++ index.php
@@ -0,0 +1,62 @@
+<meta charset="UTF-8">
+<?php
+
+// kurutemizleme sınıfı
+// kirli çamaşırlar verilip temizlenmiş olarak istenir
+
+// yıkamak, kirli çamaşır, deterjan
+// kurulamak, yıkanmış çamaşırlar (ıslak)
+// ütülemek, kurumuş çamaşırlar
+
+class KuruTemizleme{
+    // sınıf içinde kullanılacak çamaşırı sadece sınıflar içinde kullanılacak şekilde tanımlıyoruz
+    protected $camasir;
+
+    // sınıf içinde çağırılacak ama hiçbir zaman değişmeyecek sabiti tanımlıyoruz
+    const deterjan = "Ariel";
+
+    // sınıf camasir değişkenine muhtaç olduğundan bu değişkene değer atayabilmek için methodumuzu yazıyoruz
+    public function setCamasir($gelenCamasir){
+        $this->camasir = $gelenCamasir;
+    }
+
+    // sınıfa işlem yaptırabilmek için dışarıdan çağırılacak methodu tanımlıyoruz
+    public function yika($gelenCamasir = NULL){
+
+        // istersek bu fonksiyon ile çamaşırı üstüne yazabiliyoruz, kullanırken daha düzgün gözükmesi için
+        if(!is_null($gelenCamasir))
+            $this->setCamasir($gelenCamasir);
+
+        // elimizde camasir yoksa üzüntüden ölüyoruz
+        if(!$this->camasir) die("Çamaşır yok :(");
+
+        // elimizde çamaşır olduğuna göre kuru temizleme aşamalarında yapılacak işlemleri çağırıyoruz
+        $this->yikamaIslemi();
+        $this->kurulamaIslemi();
+        $this->utulemeIslemi();
+    }
+
+    // çamaşır makinesi işlemleri, deterjan sabit olarak geliyor
+    private function yikamaIslemi(){
+        echo $this->camasir . ", " . $this::deterjan . " ile yıkandı<br>";
+    }
+
+    // kurutma makinesi işlemleri
+    private function kurulamaIslemi(){
+        echo $this->camasir . " kurulandı<br>";
+    }
+
+    // ütü işlemleri
+    private function utulemeIslemi(){
+        echo $this->camasir . " ütülendi<br>";
+    }
+}
+
+// instance oluşturuyoruz
+$kuruTemizlemeci = new KuruTemizleme();
+
+// camasir için veri gönderiyoruz
+$kuruTemizlemeci->setCamasir("Pantul");
+
+// camasir yıkama işlemlerini çalıştırıyoruz
+$kuruTemizlemeci->yika();
